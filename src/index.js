2239d50d14f6c05704c78b7d546ef0d64300b205
Klayder <anderysorokin@gmail.com>
2018-02-20 15:04:44 +0300

    fix
    stay check only 5s

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 module.exports = function getZerosCount(number) {
 
 var conterTwo=0,conterFive=0;;
-  
- 	if(!(number%2 == 0)){
-  	var k=1;
-  }else{
-  	k=2;
-  }
-  //alert(number+" "+k);
+
+  k=5;
   	while(k<number || k==number){
    		var tempKFive=k;
-      var tempKTwo=k;
+    
       	while(tempKFive%5 === 0){
         		conterFive++;
       			tempKFive/=5;
         }
-        while(tempKFive%2 === 0){
-        		conterTwo++;
-      			tempKFive/=2;
-        }
+        
 				
-      k+=1;
+      k+=5;
     }
 
-return conterFive<conterTwo?conterFive:conterTwo;
+
+
+
+
+
+return conterFive;
 }
