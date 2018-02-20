4df942697430493d134eb3b377e1a11f7b5648a9
Klayder <anderysorokin@gmail.com>
2018-02-20 14:14:06 +0300

    fixed previous solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+var conterTwo=0,conterFive=0;;
+  
+ 	if(!(number%2 == 0)){
+  	var k=1;
+  }else{
+  	k=2;
+  }
+  //alert(number+" "+k);
+  	while(k<number || k==number){
+   		var tempKFive=k;
+      var tempKTwo=k;
+      	while(tempKFive%5 === 0){
+        		conterFive++;
+      			tempKFive/=5;
+        }
+        while(tempKFive%2 === 0){
+        		conterTwo++;
+      			tempKFive/=2;
+        }
+				
+      k+=1;
+    }
+
+return conterFive<conterTwo?conterFive:conterTwo;
 }
